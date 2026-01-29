b97ba3bd5d696b09eed18c716dd5686d4085b13f
dongzzi101 <dongzzi101@gmail.com>
2026-01-29 19:50:35 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 9.06 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,12 @@
+def solution(n,a,b):
+    
+    count = 0
+    
+    while a != b:
+        a = (a+1) // 2
+        b = (b+1) // 2
+        count += 1
+    
+    answer = count
+    
+    return answer
