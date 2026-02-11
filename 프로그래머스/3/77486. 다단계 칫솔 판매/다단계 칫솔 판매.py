81ce8151fa2c56a01a53ac2ace579c39c5b9cb9d
dongzzi101 <dongzzi101@gmail.com>
2026-02-11 09:08:37 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 177.72 ms, Memory: 19.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -0,0 +1,32 @@
+def query(name, price):
+    global money, tree
+
+    price10 = int(price * 0.1)
+    money[name] += price - price10
+
+    if price10 == 0:
+        return
+
+    if name != '-':
+        query(tree[name], price10)
+
+
+
+def solution(enroll, referral, seller, amount):
+    global money, tree
+
+    money = dict()
+    
+    money['-'] = 0
+    for name in enroll:
+        money[name] = 0
+
+    tree = dict()
+    for child, parent in zip(enroll, referral):
+        tree[child] = parent
+
+    for name, num in zip(seller, amount):
+        query(name, num * 100)
+
+    answer = [money[name] for name in enroll]
+    return answer
