b292446dc7e872d029d6bd94a4f67d2adb77f55c
dongzzi101 <dongzzi101@gmail.com>
2026-02-19 09:07:53 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,46 +1,44 @@
 from collections import deque
 
-N = int(input())
-matrix = [list(map(int, input())) for _ in range(N)]
 
-visited = [[False] * N for _ in range(N)]
-
-dy = [1, 0, -1, 0]
-dx = [0,1, 0,-1]
-
-
-
-def bfs(sy, sx):
+def bfs(si, sj):
 	q = deque()
-	q.append((sy, sx))
-	visited[sy][sx] = True  
+	q.append((si,sj))
+	vistited[si][sj] = True	
 	count = 1
 
 	while q:
-		y, x = q.popleft()
+		ci, cj = q.popleft()
 
-		for d in range(4):
-			ny = y + dy[d]
-			nx = x + dx[d]
+		for di, dj in ((-1,0), (1,0), (0,-1), (0,1)):
+			ni, nj = ci+di, cj+dj
 
-			if 0 <= ny < N and 0 <= nx < N:
-				if matrix[ny][nx] == 1 and not visited[ny][nx]:
-					visited[ny][nx] = True
-					q.append((ny, nx))
+			if (0<= ni < N and 0 <= nj < N):
+				if matrix[ni][nj] == 1 and  not vistited[ni][nj]:
+					q.append((ni, nj))
+					vistited[ni][nj] = True
 					count += 1
 	return count
 
-result = []
+
+N = int(input())
+matrix = [ list(map(int, input())) for _ in range(N) ]
+
+vistited = [[0] * N for _ in range(N)]
+ans = []
 
 for i in range(N):
-    for j in range(N):
-        if matrix[i][j] == 1 and not visited[i][j]:
-            result.append(bfs(i, j))
+	for j in range(N):
+		if matrix[i][j] == 1 and vistited[i][j] == 0:
+			ans.append(bfs(i, j))
 
-result.sort()
-print(len(result))
-for r in result:
-	print(r)
+ans.sort()
+print(len(ans), *ans, sep='\n')
+
+
+
+
+
 
 
 
