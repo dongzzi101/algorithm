831e83c02a3c2296ee556395dcfda4248fcfd67e
dongzzi101 <dongzzi101@gmail.com>
2026-02-22 11:35:21 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 348 ms, Memory: 67000 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,34 +1,34 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+
+
+
+def dfs(idx):
+	global visited, answer, adjs
+
+	visited[idx] = True
+
+	for adj in adjs[idx]:		
+		if not visited[adj]:
+			answer[adj] = idx
+			visited[adj] = True
+			dfs(adj)
+
 
 N = int(input())
 
-adjs_list = [[] for _ in range(N+1)]
+adjs = [[] for _ in range(N+1)]
+answer = [0] * (N+1)
+visited = [False] * (N+1)
 
 for _ in range(N-1):
 	a, b = map(int, input().split())
-	adjs_list[a].append(b)
-	adjs_list[b].append(a)
+	adjs[a].append(b)
+	adjs[b].append(a)
 
-visited = [False] * (N+1)
-parent = [0] * (N+1)
-
-q = deque()
-q.append(1)
-visited[1] = True
-
-
-while q:
-	node = q.popleft()
-
-
-	for cur_node in adjs_list[node]:
-
-		if visited[cur_node]:
-			continue
-
-		parent[cur_node] = node
-		q.append(cur_node)
-		visited[cur_node] = True
+dfs(1)
 
 for i in range(2, N+1):
-	print(parent[i])
+	print(answer[i])
