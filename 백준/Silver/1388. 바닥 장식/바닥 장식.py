b69680358a5a01e4b2599f8470c1241db4e038aa
dongzzi101 <dongzzi101@gmail.com>
2026-02-23 10:39:52 +0900

    [Silver IV] Title: 바닥 장식, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.py
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -4,44 +4,33 @@ input = sys.stdin.readline
 
 
 def dfs(y, x):
+    visited[y][x] = True
 
-	global visited, map_
+    if map_[y][x] == '-':
+        if x + 1 <= M and not visited[y][x+1] and map_[y][x+1] == '-':
+            dfs(y, x+1)
 
-	visited[y][x] = True
-
-	if map_[y][x] == 0:
-		nx = x + 1
-		if nx <= M and not visited[y][nx] and map_[y][nx] == 0:
-			dfs(y, nx)
-
-	else:
-		ny = y + 1
-		if ny <= N and not visited[ny][x] and map_[ny][x] == 1:
-			dfs(ny, x)
-
+    elif map_[y][x] == '|':
+        if y + 1 <= N and not visited[y+1][x] and map_[y+1][x] == '|':
+            dfs(y+1, x)
 
 
 N, M = map(int, input().split())
 
-map_ = [[False] * (M+1) for _ in range(N+1)]
+map_ = [[0] * (M+1) for _ in range(N+1)]
 visited = [[False] * (M+1) for _ in range(N+1)]
+
+for y in range(1, N+1):
+    row = input().strip()
+    for x in range(1, M+1):
+        map_[y][x] = row[x-1]
+
 answer = 0
 
 for y in range(1, N+1):
-	row = input()
-	for x in range(1, M+1):
-		if row[x-1] == "-":
-			map_[y][x] = 0
-		else:
-			map_[y][x] = 1
-
-
-for y in range(1, N+1):
-	for x in range(1, M+1):
-		if not visited[y][x]:
-			dfs(y, x)
-			answer += 1
+    for x in range(1, M+1):
+        if not visited[y][x]:
+            dfs(y, x)
+            answer += 1
 
 print(answer)
-
-
