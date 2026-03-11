3c9424916298053766d4bae9517b26bc94f89c6f
dongzzi101 <dongzzi101@gmail.com>
2026-03-11 18:51:56 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,26 +1,29 @@
 from collections import deque
 
-dy = [-1,0,1,0]
-dx = [0,1,0,-1]
-
 N, M = map(int, input().split())
-matrix = ['0' *  (M+1)] + ['0' + input() for _ in range(N)]
+maps = [ list(map(int, input()))  for _ in range(N)]
+
+
+visited = [[False] * M for _ in range(N)]
 
 q = deque()
-visited = [[False] * (M+1) for _ in range(N+1)]
+q.append((0,0))
 
-q.append((1,1,1))
-visited[1][1] = True
+visited[0][0] = True
+
+dy = [1, 0, -1, 0]
+dx = [0, 1, 0, -1]
 
 while q:
-    dist, y, x = q.popleft()
-    if y == N and x == M:
-    	print(dist)
-    	exit()
+	y, x = q.popleft()
 
-    for i in range(4):
-    	ny = y + dy[i]
-    	nx = x + dx[i]
-    	if (1 <= ny <= N and 1 <= nx <= M) and (not visited[ny][nx]) and (matrix[ny][nx] == '1'): 
-    		q.append((dist+1, ny, nx))
-    		visited[ny][nx] = True
+	for i in range(4):
+		ny = y + dy[i]
+		nx = x + dx[i]
+
+		if 0 <= nx < M and 0 <= ny < N:
+			if maps[ny][nx] == 1:
+				maps[ny][nx] = maps[y][x] + 1
+				q.append((ny, nx))
+
+print(maps[N-1][M-1])
