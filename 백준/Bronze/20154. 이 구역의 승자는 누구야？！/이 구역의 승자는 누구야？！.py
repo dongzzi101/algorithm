34d68d3603704bdcbe65f9fc19bce5364a97c2df
dongzzi101 <dongzzi101@gmail.com>
2026-03-13 17:59:36 +0900

    [Bronze I] Title: 이 구역의 승자는 누구야?!, Time: 280 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/20154. 이 구역의 승자는 누구야？！/이 구역의 승자는 누구야？！.py
+++ 백준/Bronze/20154. 이 구역의 승자는 누구야？！/이 구역의 승자는 누구야？！.py
@@ -1,16 +1,9 @@
-score = {
-    'A': 3, 'B': 2, 'C': 1, 'D': 2, 'E': 3, 'F': 3, 'G': 3,
-    'H': 3, 'I': 1, 'J': 1, 'K': 3, 'L': 1, 'M': 3, 'N': 3,
-    'O': 1, 'P': 2, 'Q': 2, 'R': 2, 'S': 1, 'T': 2,
-    'U': 1, 'V': 1, 'W': 2, 'X': 2, 'Y': 2, 'Z': 1
-}
+score = [3,2,1,2,3,3,3,3,1,1,3,1,3,3,1,2,2,2,1,2,1,1,2,2,2,1]
 
 s = input()
-result = 0
-for ch in s:
-    result += score[ch]
 
-if result % 2 == 0:
-    print("You're the winner?")
-else:
-    print("I'm a winner!")
+sum = 0
+for ch in s:
+	sum += score[ord(ch) - ord('A')]
+
+print("I'm a winner!" if sum % 2 != 0 else "You're the winner?")
