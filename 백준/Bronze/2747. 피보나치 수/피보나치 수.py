966ced164afc20471350489fe93bccb937b187ce
dongzzi101 <dongzzi101@gmail.com>
2026-03-16 23:59:49 +0900

    [Bronze II] Title: 피보나치 수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+dp = [0] * (N+1)
+
+
+if N >= 0:
+	dp[0] = 0
+
+if N >= 1:
+	dp[1] = 1
+
+if N >= 2:
+	dp[2] = 1
+
+for i in range(3, N+1):
+	dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[N])
