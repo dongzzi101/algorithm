3048204a5b06aab42f75d079ebc1765f29b3f27a
dongzzi101 <dongzzi101@gmail.com>
2026-03-17 20:32:18 +0900

    [Silver I] Title: 오르막 수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -1,12 +1,9 @@
 n = int(input())
 
-dp = [[0] * (10) for _ in range(n+1)]
-
-dp[1] = [1] * 10
+dp = [1] * 10
 
 for i in range(2, n+1):
 	for j in range(10):
-		dp[i][j] = sum(dp[i-1][j:])
+		dp[j] = sum(dp[j:])
 
-
-print(sum(dp[n]) % 10007)
+print(sum(dp) % 10007)
