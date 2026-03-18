3bc151a84fb8610ac72adda9f5734661a48e5689
dongzzi101 <dongzzi101@gmail.com>
2026-03-19 08:28:03 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,11 +1,14 @@
-N = int(input())
-count = 0
+n = int(input())
 
-while N >= 0:
-    if N % 5 == 0:
-        print(count + N // 5)
-        break
-    N -= 3
-    count += 1
-else:
-    print(-1)
+INF = 1e9
+dp = [INF] * (n+1)
+
+dp[0] = 0
+
+for i in range(3, n+1):
+    if i - 3 >= 0:
+        dp[i] = min(dp[i], dp[i-3] + 1)
+    if i - 5 >= 0:
+        dp[i] = min(dp[i], dp[i-5] + 1)
+
+print(dp[n] if dp[n] != INF else -1)
