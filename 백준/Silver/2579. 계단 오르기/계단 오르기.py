2da8976feb95b492ac7473af23e938958ae5e75b
dongzzi101 <dongzzi101@gmail.com>
2026-03-20 09:52:41 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,16 +1,14 @@
-stairs = int(input())
-arr = [int(input()) for _ in range(stairs)]
+N = int(input())
 
-dp = [[-1e9] * 3 for _ in range(stairs)]
+arr = [0] + [int(input()) for _ in range(N)]
 
-dp[0][1] = arr[0]
+dp = [[0] * 3 for _ in range(N+1)]
 
-if stairs > 1:
-    dp[1][1] = arr[1]
-    dp[1][2] = arr[0] + arr[1]
 
-for i in range(2, stairs):
-    dp[i][1] = max(dp[i-2][1], dp[i-2][2]) + arr[i]
-    dp[i][2] = dp[i-1][1] + arr[i]
+for i in range(1, N+1):
+	dp[i][0] = max(dp[i-1][1], dp[i-1][2])
+	dp[i][1] = dp[i-1][0] + arr[i]
+	dp[i][2] = dp[i-1][1] + arr[i]
 
-print(max(dp[stairs-1][1], dp[stairs-1][2]))
+print(max(dp[N][1], dp[N][2]))
+
