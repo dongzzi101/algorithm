98b410a8b026238eb6dcb014f4a5f9814d71f68e
dongzzi101 <dongzzi101@gmail.com>
2026-03-22 07:24:21 +0900

    [Silver I] Title: RGB거리, Time: 56 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,15 +1,12 @@
 N = int(input())
 
-arr = [[0] * N]
-arr += [ list(map(int, input().split())) for _ in range(N)]
-
+arr = [[0,0,0]] + [list(map(int, input().split())) for _ in range(N)]
 dp = [[0] * 3 for _ in range(N+1)]
-
 dp[1] = arr[1]
 
 for i in range(2, N+1):
-	dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + arr[i][0]
-	dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + arr[i][1]
+	dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + arr[i][0] 
+	dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + arr[i][1] 
 	dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + arr[i][2]
 
 print(min(dp[N]))
