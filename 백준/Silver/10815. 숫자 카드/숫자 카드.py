024e74a6f8ccd1b64d7faef9a2a3c5f110d467a5
dongzzi101 <dongzzi101@gmail.com>
2026-03-27 19:27:09 +0900

    [Silver V] Title: 숫자 카드, Time: 1868 ms, Memory: 109828 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,27 +1,29 @@
-def is_exist(arr, num):
-	left = 0
-	right = len(arr) - 1
-
-	while left <= right:
-		mid = (left + right) // 2
-
-		if arr[mid] < num: 
-			left = mid + 1
-
-		if arr[mid] > num: 
-			right = mid - 1
-
-		if arr[mid] == num: 
-			return 1
-
-	return 0
-
-
 N = int(input())
-arr = sorted(list(map(int, input().split())))
+cards = list(map(int, input().split()))
 
 M = int(input())
-nums = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-for num in nums:
-	print(is_exist(arr, num), end=" ")
+cards.sort()
+
+def is_exist(target):
+    left = 0
+    right = len(cards) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if target < cards[mid]:
+            right = mid - 1
+        elif target > cards[mid]:
+            left = mid + 1
+        else:
+            return 1
+
+    return 0
+
+answer = []
+for num in arr:
+	answer.append(is_exist(num))
+
+print(*answer)
