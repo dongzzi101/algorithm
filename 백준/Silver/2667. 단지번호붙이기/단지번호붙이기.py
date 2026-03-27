851948058e7ad39c5f5bd02cbb29acced30fe7cc
dongzzi101 <dongzzi101@gmail.com>
2026-03-27 19:03:46 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,44 +1,44 @@
 from collections import deque
 
+n = int(input())
 
-def bfs(si, sj):
-	q = deque()
-	q.append((si,sj))
-	vistited[si][sj] = True	
-	count = 1
+board = [[0]*(n+1)]
+for _ in range(n):
+    board.append([0] + list(map(int, input())))
 
-	while q:
-		ci, cj = q.popleft()
+visited = [[False]*(n+1) for _ in range(n+1)]
 
-		for di, dj in ((-1,0), (1,0), (0,-1), (0,1)):
-			ni, nj = ci+di, cj+dj
+dy = [1, 0, -1, 0]
+dx = [0, 1, 0, -1]
 
-			if (0<= ni < N and 0 <= nj < N):
-				if matrix[ni][nj] == 1 and  not vistited[ni][nj]:
-					q.append((ni, nj))
-					vistited[ni][nj] = True
-					count += 1
-	return count
+def bfs(y, x):
+    q = deque([(y, x)])
+    visited[y][x] = True
+    count = 1
 
+    while q:
+        y, x = q.popleft()
 
-N = int(input())
-matrix = [ list(map(int, input())) for _ in range(N) ]
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
 
-vistited = [[0] * N for _ in range(N)]
-ans = []
+            if 1 <= ny <= n and 1 <= nx <= n:
+                if not visited[ny][nx] and board[ny][nx] == 1:
+                    visited[ny][nx] = True
+                    q.append((ny, nx))
+                    count += 1
+    return count
 
-for i in range(N):
-	for j in range(N):
-		if matrix[i][j] == 1 and vistited[i][j] == 0:
-			ans.append(bfs(i, j))
+count_list = []
 
-ans.sort()
-print(len(ans), *ans, sep='\n')
+for y in range(1, n+1):
+    for x in range(1, n+1):
+        if board[y][x] == 1 and not visited[y][x]:
+            count_list.append(bfs(y, x))
 
+count_list.sort()
 
-
-
-
-
-
-
+print(len(count_list))
+for c in count_list:
+    print(c)
