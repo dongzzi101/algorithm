59b4e32cd94f5a0e15aa77b5ce0a516ebdfd5fd5
dongzzi101 <dongzzi101@gmail.com>
2026-04-03 18:06:33 +0900

    [Silver I] Title: 미로 탐색, Time: 96 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -2,33 +2,29 @@ from collections import deque
 
 n, m = map(int, input().split())
 
-board = [[0]*(m+1)]
-for _ in range(n):
-    board.append([0] + list(map(int, input())))
+maze = [list(map(int, input())) for _ in range(n)]
+
+q = deque()
+q.append((0,0,1))
+
+visited = [[False] * (m) for _ in range(n)]
+visited[0][0] = True
 
 dy = [1, 0, -1, 0]
 dx = [0, 1, 0, -1]
 
-q = deque()
-q.append((1,1))
-
-visited = [[False] * (m+1) for _ in range(n+1)]
-visited[1][1] = True
-
-dist = [[0]*(m+1) for _ in range(n+1)]
-dist[1][1] = 1
-
 while q:
-	y, x = q.popleft()
+	y, x, dist = q.popleft()
+
+	if (y == n-1) and (x == m-1):
+		print(dist)
 
 	for i in range(4):
 		ny = y + dy[i]
 		nx = x + dx[i]
 
-		if (1 <= ny <= n) and (1 <= nx <= m):
-			if not visited[ny][nx] and board[ny][nx] == 1:
-				q.append((ny, nx))
-				visited[ny][nx] = True
-				dist[ny][nx] = dist[y][x] + 1
-
-print(dist[n][m])
+		if (0<= ny < n) and (0<= nx < m):
+			if not visited[ny][nx]:
+				if maze[ny][nx] == 1:
+					q.append((ny, nx, dist+1))
+					visited[ny][nx] = True
