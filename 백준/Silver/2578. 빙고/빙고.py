9c1d178b435bff1f418ec2c663285452a7826096
dongzzi101 <dongzzi101@gmail.com>
2026-04-03 15:16:06 +0900

    [Silver IV] Title: 빙고, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,38 +1,49 @@
-import sys
+bingo_pan = [list(map(int, input().split())) for _ in range(5)]
 
-board = [list(map(int, input().split())) for _ in range(5)]
-checked = [[0] * 5 for _ in range(5)]
+call_number = []
 
-count = 0
 for _ in range(5):
-    call = list(map(int, input().split()))
-    for num in call:
+	call_number += list(map(int, input().split()))
+
+
+def change_num_in_bingo_pan(num):
+	for y in range(5):
+		for x in range(5):
+			if bingo_pan[y][x] == num:
+				bingo_pan[y][x] = 0
+
+def check_count_bingo():
+    count = 0
+
+    # 가로
+    for y in range(5):
+        if all(bingo_pan[y][x] == 0 for x in range(5)):
+            count += 1
+
+    # 세로
+    for x in range(5):
+        if all(bingo_pan[y][x] == 0 for y in range(5)):
+            count += 1
+
+    # 대각선
+    if all(bingo_pan[i][i] == 0 for i in range(5)):
         count += 1
 
-        for i in range(5):
-            for j in range(5):
-                if board[i][j] == num:
-                    checked[i][j] = 1
+    if all(bingo_pan[i][4-i] == 0 for i in range(5)):
+        count += 1
 
-        line = 0
+    return count
 
-        for i in range(5):
-            if sum(checked[i]) == 5:
-                line += 1
+	
+		
 
-        for j in range(5):
-            s = 0
-            for i in range(5):
-                s += checked[i][j]
-            if s == 5:
-                line += 1
+call_count = 0
+bingo_count = 3
 
-        if checked[0][0] + checked[1][1] + checked[2][2] + checked[3][3] + checked[4][4] == 5:
-            line += 1
-
-        if checked[0][4] + checked[1][3] + checked[2][2] + checked[3][1] + checked[4][0] == 5:
-            line += 1
-
-        if line >= 3:
-            print(count)
-            sys.exit()
+for num in call_number:
+    call_count += 1
+    change_num_in_bingo_pan(num)
+    
+    if check_count_bingo() >= 3:
+        print(call_count)
+        break
