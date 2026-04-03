beb4ca954eddbad5f98b72bfa5d7d2aba4815602
dongzzi101 <dongzzi101@gmail.com>
2026-04-03 14:37:13 +0900

    [Bronze II] Title: 전구, Time: 1468 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/21918. 전구/전구.py
+++ 백준/Bronze/21918. 전구/전구.py
@@ -1,24 +1,26 @@
-import sys
+N, M = map(int, input().split())
 
-n, m = map(int, input().split())
-lights = [0] + list(map(int, input().split()))
+bulbs = [0] + list(map(int, input().split()))
 
-for _ in range(m):
-    a, b, c = map(int, input().split())
+for _ in range(M):
+	a, b, c = map(int, input().split())
 
-    if a == 1:
-        lights[b] = c
+	if a == 1:
+		bulbs[b] = c
+	
+	elif a == 2:
+		for i in range(b, c+1):
+			if bulbs[i] == 0:
+				bulbs[i] = 1
+			else:
+				bulbs[i] = 0
+	
+	elif a == 3:
+		for i in range(b, c+1):
+			bulbs[i] = 0
+		
+	elif a == 4:
+		for i in range(b, c+1):
+			bulbs[i] = 1
 
-    elif a == 2:
-        for i in range(b, c + 1):
-            lights[i] = 1 - lights[i]
-
-    elif a == 3:
-        for i in range(b, c + 1):
-            lights[i] = 0
-
-    elif a == 4:
-        for i in range(b, c + 1):
-            lights[i] = 1
-
-print(' '.join(map(str, lights[1:])))
+print(*bulbs[1:])
