2b31799daab7e98aeb832cc5dfe5e1e58ae1241b
dongzzi101 <dongzzi101@gmail.com>
2026-04-03 17:11:16 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 708 ms, Memory: 41664 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -2,45 +2,49 @@ from collections import deque
 
 n, m = map(int, input().split())
 
-board = [ [0] + list(map(int, input().split()))  for _ in range(n)]
+
+lands = [list(map(int, input().split())) for _ in range(n)]
+
+def find_number_two():
+	for y in range(n):
+		for x in range(m):
+			if lands[y][x] == 2:
+				return y, x
+
+
+q = deque()
+y, x = find_number_two()
+lands[y][x] = 0
+q.append((y, x))
+
+
+visited = [[False] * m for _ in range(n)]
+visited[y][x] = True
+
 
 dy = [1, 0, -1, 0]
 dx = [0, 1, 0, -1]
 
-def find_target(num):
-    for y in range(len(board)):
-        for x in range(len(board[y])):
-            if board[y][x] == 2:
-                return y, x
-
-y, x = find_target(2)
-
-q = deque()
-q.append((y, x))
-
-dist = [[-1]*(m+1) for _ in range(n+1)]
-visited = [[False] * (m+1) for _ in range(n+1)]
-
-visited[y][x] = True
-dist[y][x] = 0
-
 while q:
-    y, x = q.popleft()
 
-    for i in range(4):
-        ny = y + dy[i]  
-        nx = x + dx[i]  
+	y, x = q.popleft()
 
-        if 0 <= ny < n and 1 <= nx <= m:  
-            if board[ny][nx] != 0 and not visited[ny][nx]:  
-                dist[ny][nx] = dist[y][x] + 1
-                visited[ny][nx] = True
-                q.append((ny, nx))
+	for i in range(4):
+		ny = y + dy[i]
+		nx = x + dx[i]
 
-for i in range(n):
-    for j in range(1, m+1):
-        if board[i][j] == 0:
-            print(0, end=' ')
-        else:
-            print(dist[i][j], end=' ')
-    print()
+		if (0 <= ny < n) and (0 <= nx < m):
+			if not visited[ny][nx]:
+				if lands[ny][nx] != 0:
+					lands[ny][nx] = (lands[y][x] + 1)
+					q.append((ny, nx))
+					visited[ny][nx] = True
+
+for y in range(n):
+    for x in range(m):
+        if lands[y][x] == 1 and not visited[y][x]:
+            lands[y][x] = -1	
+
+for land in lands:
+	print(*land)
+
