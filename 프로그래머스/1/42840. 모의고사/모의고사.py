bbadbe37fe4917094144084ff13891f5bbfc5efa
dongzzi101 <dongzzi101@gmail.com>
2026-04-05 00:08:58 +0900

    [level 1] Title: 모의고사, Time: 5.45 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,29 +1,16 @@
 def solution(answers):
-    answer = []
+    patterns =[
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ]
     
-    student1 = [1, 2, 3, 4, 5]
-    student2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    scores = [0] * 3
     
-    student1_answer_count = 0
-    student2_answer_count = 0
-    student3_answer_count = 0
-    
-    scores = [] 
-    
-    for i in range(len(answers)):
-        if answers[i] == student1[i % len(student1)]:
-            student1_answer_count += 1
-        
-        if answers[i] == student2[i % len(student2)]:
-            student2_answer_count += 1
-        
-        if answers[i] == student3[i % len(student3)]:
-            student3_answer_count += 1
-    
-    scores.append(student1_answer_count)
-    scores.append(student2_answer_count)
-    scores.append(student3_answer_count)
+    for i, answer in enumerate(answers):
+        for j, pattern in enumerate(patterns):
+            if answer == pattern[i % len(pattern)]:
+                scores[j] += 1
     
     max_score = max(scores)
     highest_score = []
