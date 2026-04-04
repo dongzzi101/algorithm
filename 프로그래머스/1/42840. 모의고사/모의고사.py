c4fd490874a761e88681e24fc7a58473cf4a8bde
dongzzi101 <dongzzi101@gmail.com>
2026-04-05 00:06:00 +0900

    [level 1] Title: 모의고사, Time: 6.27 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,30 +1,35 @@
 def solution(answers):
     answer = []
-    n = len(answers)
     
-    supo1 = [1, 2, 3, 4, 5]
-    supo2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    supo3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-
-    supo1_count = 0
-    supo2_count = 0
-    supo3_count = 0
-
+    student1 = [1, 2, 3, 4, 5]
+    student2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    student1_answer_count = 0
+    student2_answer_count = 0
+    student3_answer_count = 0
+    
+    scores = [] 
+    
     for i in range(len(answers)):
-        if supo1[i % len(supo1)] == answers[i]:
-            supo1_count += 1
-
-        if supo2[i % len(supo2)] == answers[i]:
-            supo2_count += 1
-
-        if supo3[i % len(supo3)] == answers[i]:
-            supo3_count += 1
+        if answers[i] == student1[i % len(student1)]:
+            student1_answer_count += 1
+        
+        if answers[i] == student2[i % len(student2)]:
+            student2_answer_count += 1
+        
+        if answers[i] == student3[i % len(student3)]:
+            student3_answer_count += 1
     
-    scores =[supo1_count, supo2_count, supo3_count]
+    scores.append(student1_answer_count)
+    scores.append(student2_answer_count)
+    scores.append(student3_answer_count)
+    
     max_score = max(scores)
-
-    for i in range(3):
-        if scores[i] == max_score:
-            answer.append(i + 1)
-
-    return answer
+    highest_score = []
+    
+    for i, score in enumerate(scores):
+        if score == max_score:
+            highest_score.append(i+1)
+    
+    return highest_score
