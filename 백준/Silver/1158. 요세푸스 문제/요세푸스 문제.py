004b23a4900677df09f42ce31bfc0c0c3998d7c2
dongzzi101 <dongzzi101@gmail.com>
2026-04-07 00:17:35 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 2280 ms, Memory: 34908 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+N, K = map(int, input().split())
+q = deque(range(1, N+1))
+
+result = []
+
+while q:
+	for _ in range(K-1):
+		q.append(q.popleft())
+
+	result.append(q.popleft())
+
+
+print("<" + ", ".join(map(str, result)) + ">")
