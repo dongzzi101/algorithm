275d04037803bcf4d667af4fc7e1d20a3f57f05b
dongzzi101 <dongzzi101@gmail.com>
2026-04-07 22:03:38 +0900

    [Silver III] Title: 파일 정리, Time: 1412 ms, Memory: 41456 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -1,13 +1,16 @@
 N = int(input())
 
 extends_map = {}
-for _ in range(N):
-	name, extend = input().split(".")
 
-	if extend in extends_map:
-		extends_map[extend] += 1
+for _ in range(N):
+	name, extends = input().split(".")
+
+	if extends in extends_map:
+		extends_map[extends] += 1
 	else:
-		extends_map[extend] = 1
+		extends_map[extends] = 1
+
+
 
 
 for i in sorted(extends_map):
