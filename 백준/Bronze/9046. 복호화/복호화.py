89efbb5851baad38b7e8decd0a6573af53e403d2
dongzzi101 <dongzzi101@gmail.com>
2026-04-07 22:18:01 +0900

    [Bronze II] Title: 복호화, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9046. 복호화/복호화.py
+++ 백준/Bronze/9046. 복호화/복호화.py
@@ -1,17 +1,25 @@
 T = int(input())
+
+
 for _ in range(T):
-    s = input().strip()
+	word_map = {}
+	s = input()
 
-    count = [0] * 26
+	for ch in s:
+		if ch == ' ':
+			continue
+			
+		if ch in word_map:
+			word_map[ch] += 1
+		else:
+			word_map[ch] = 1
 
-    for ch in s:
-        if 'a' <= ch <= 'z':
-            count[ord(ch) - ord('a')] += 1
+	max_value = max(word_map.values())
 
-    max_count = max(count)
+	max_keys = [k for k, v in word_map.items() if v == max_value]
 
-    if count.count(max_count) > 1:
-        print('?')
-    else:
-        idx = count.index(max_count)
-        print(chr(idx + ord('a')))
+	if len(max_keys) > 1:
+		print("?")
+	else:
+		print(max_keys[0])
+
