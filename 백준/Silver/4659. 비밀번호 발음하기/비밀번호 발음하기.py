60e19d6fb880ab46392dd798b244f052022a49be
dongzzi101 <dongzzi101@gmail.com>
2026-04-09 18:00:16 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -1,43 +1,78 @@
-vowels = ['a', 'e', 'i', 'o', 'u']
+
+
+def include_vowel(word):
+	
+
+	for ch in word:
+		if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+			return True
+	return False
+
+def three_in_a_row(word):
+	const_count = 0
+	vowel_count = 0
+
+	for ch in word:
+		if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+			vowel_count += 1
+			const_count = 0
+			
+			if vowel_count == 3:
+				return False
+		
+		else:
+			const_count += 1
+			vowel_count = 0
+
+			if const_count == 3:
+				return False
+
+	return True
+
+def two_in_a_row_except_e_and_o(word):
+	same_o_e_count = 0
+
+	prev = word[0]
+
+	for i in range(1, len(word)):
+		if prev == word[i]:
+			if word[i] == "e" or word[i] == "o":
+				same_o_e_count += 1
+				if same_o_e_count == 2:
+					return False
+				prev = word[i]
+			else:
+				return False
+
+		else:
+			same_o_e_count = 0
+			prev = word[i]
+
+	return True
+
+
+
+
+
+
 while True:
-    password = input()
+	word = input()
+	if word == "end":
+		break
 
-    if password == 'end':
-        break
+	include_vowel_reuslt = include_vowel(word)
 
-    has_vowel = False
-    vowel_cnt = 0
-    cons_cnt = 0
-    prev = ''
-    acceptable = True
+	# if not include_vowel_reuslt:
+	# 	print(f"<{word}> is not acceptable.")
 
-    for ch in password:
-        if ch in vowels:
-            has_vowel = True
-            vowel_cnt += 1
-            cons_cnt = 0
-        else:
-            cons_cnt += 1
-            vowel_cnt = 0
+	three_in_a_row_result = three_in_a_row(word)
 
-        if vowel_cnt == 3 or cons_cnt == 3:
-            acceptable = False
-            break
+	# if not three_in_a_row_result:
+	# 	print(f"<{word}> is not acceptable.")
 
-        if prev == ch and ch not in ['e', 'o']:
-            acceptable = False
-            break
+	two_in_a_row_except_e_and_o_result = two_in_a_row_except_e_and_o(word)
 
-        prev = ch
-
-    if not has_vowel:
-        acceptable = False
-
-    if acceptable:
-        print(f"<{password}> is acceptable.")
-    else:
-        print(f"<{password}> is not acceptable.")
-
-
-
-
+	if include_vowel_reuslt and three_in_a_row_result and two_in_a_row_except_e_and_o_result:
+		print(f"<{word}> is acceptable.")
+	else:
+		print(f"<{word}> is not acceptable.")
