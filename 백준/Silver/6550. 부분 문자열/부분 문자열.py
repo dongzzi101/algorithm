5ba2088bac1112587210b436658c6f873bed50d9
dongzzi101 <dongzzi101@gmail.com>
2026-04-09 18:36:49 +0900

    [Silver V] Title: 부분 문자열, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/6550. 부분 문자열/부분 문자열.py
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.py
@@ -1,18 +1,14 @@
-import sys
+while True:
+	try:
+		s, t = input().split()
+	except:
+		break
 
-for line in sys.stdin:
-    line = line.strip()
-    if not line:
-        continue
 
-    k, s = line.split()
+	i = 0
 
-    i = 0
-    for ch in s:
-        if i < len(k) and ch == k[i]:
-            i +=1
+	for ch in t:
+		if i < len(s) and ch == s[i]:
+			i += 1
 
-    if i == len(k):
-        print("Yes")
-    else:
-        print("No")
+	print("Yes" if len(s) == i else "No")
