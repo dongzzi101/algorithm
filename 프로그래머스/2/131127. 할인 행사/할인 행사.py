5669b05613c087c206a4de5ed21f363d86f18d87
dongzzi101 <dongzzi101@gmail.com>
2026-04-09 21:19:29 +0900

    [level 2] Title: 할인 행사, Time: 37.50 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,18 +1,28 @@
+from collections import defaultdict
+
 def solution(want, number, discount):
     answer = 0
-    n = sum(number) 
     
-    want_dict = {}
-    for i in range(len(want)):
-        want_dict[want[i]] = number[i]
-
-    for i in range(len(discount) - n + 1):
-        cart = {}
-
-        for j in range(i, i+n):
-            cart[discount[j]] = cart.get(discount[j], 0) + 1
-
-        if cart == want_dict:
+    want_dic = {name: number[i] for i, name in enumerate(want)}
+    window = defaultdict(int)
+    
+    for i in range(10):
+        window[discount[i]] += 1
+    
+    def is_valid():
+        for key in want_dic:
+            if window[key] != want_dic[key]:
+                return False
+        return True
+    
+    if is_valid():
+        answer += 1
+    
+    for i in range(10, len(discount)):
+        window[discount[i-10]] -= 1
+        window[discount[i]] += 1
+        
+        if is_valid():
             answer += 1
-
+    
     return answer
