8292099c735a06692db1e5b9f3430057d92b0efa
dongzzi101 <dongzzi101@gmail.com>
2026-04-13 18:43:52 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -0,0 +1,23 @@
+def solution(storey):
+    answer = 0
+
+    while storey > 0:
+        d = storey % 10
+        
+        if d < 5:
+            answer += d
+            storey //= 10
+            
+        elif d > 5:
+            answer += (10 - d)
+            storey = storey // 10 + 1
+            
+        else:  
+            if (storey // 10) % 10 >= 5:
+                answer += 5
+                storey = storey // 10 + 1
+            else:
+                answer += 5
+                storey //= 10
+    
+    return answer
