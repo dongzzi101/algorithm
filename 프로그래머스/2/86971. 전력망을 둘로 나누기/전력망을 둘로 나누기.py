d8f5165a4beeb6ceed3564f6fcfc4990a42c752a
dongzzi101 <dongzzi101@gmail.com>
2026-04-13 17:55:43 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.83 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,30 +1,7 @@
 def solution(n, wires):
-    answer = n
-    
+    answer = -1
     adjs = [[] for _ in range(n+1)]
+    print(adjs)
     
-    for a, b in wires:
-        adjs[a].append(b)
-        adjs[b].append(a)
-    
-    def dfs(node, visited, cut_a, cut_b):
-        visited[node] = True
-        count = 1
-        
-        for next in adjs[node]:
-            if (node == cut_a and next == cut_b) or (node == cut_b and next == cut_a):
-                continue
-            
-            if not visited[next]:
-                count += dfs(next, visited, cut_a, cut_b)
-        
-        return count
-    
-    for a, b in wires:
-        visited = [False] * (n+1)
-        size = dfs(1, visited, a, b)
-        
-        diff = abs(size - (n - size))
-        answer = min(answer, diff)
     
     return answer
