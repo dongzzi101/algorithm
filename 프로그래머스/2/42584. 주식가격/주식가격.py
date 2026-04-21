9a4a8ac0c28c08ec26e5cdaf819c29c0b4d4ba53
dongzzi101 <dongzzi101@gmail.com>
2026-04-21 16:04:29 +0900

    [level 2] Title: 주식가격, Time: 188.53 ms, Memory: 18.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,15 +1,13 @@
 def solution(prices):
     answer = []
     
-    for i in range(len(prices)):
+    for i in range(len(prices) - 1):
         time = 0
-        
         for j in range(i+1, len(prices)):
             time += 1
-            
-            if prices[j] < prices[i]:
+            if prices[i] > prices[j]:
                 break
         
         answer.append(time)
     
-    return answer
+    return answer + [0]
