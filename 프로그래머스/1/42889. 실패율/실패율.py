b5fcb68c9fd9b3ccbafeba33830bb6aa064485d3
dongzzi101 <dongzzi101@gmail.com>
2026-04-21 14:17:56 +0900

    [level 1] Title: 실패율, Time: 14.77 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,3 +1,25 @@
+from collections import Counter
+
 def solution(N, stages):
     answer = []
+    s_count = Counter(stages)
+    participants = len(stages)
+    
+    fail_rates = []
+    
+    for i in range(1, N+1):
+        if participants == 0:
+            fail_rate = 0
+        else:
+            fail_rate = s_count[i] / participants
+        
+        fail_rates.append((i, fail_rate))
+        participants -= s_count[i]
+    
+    fail_rates.sort(key=lambda x: (-x[1], x[0]))
+    
+    for fail_rate in fail_rates:
+        stage, _ = fail_rate
+        answer.append(stage)
+    
     return answer
