d3884ee3943185f5c2539f21ea50cb6037417b0b
dongzzi101 <dongzzi101@gmail.com>
2026-04-21 13:37:43 +0900

    [level 1] Title: 모의고사, Time: 4.88 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,10 +1,10 @@
 def solution(answers):
-    patterns =[
+    answer = []
+    patterns = [
         [1, 2, 3, 4, 5],
         [2, 1, 2, 3, 2, 4, 2, 5],
         [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     ]
-    
     scores = [0] * 3
     
     for i, answer in enumerate(answers):
@@ -14,7 +14,6 @@ def solution(answers):
     
     max_score = max(scores)
     highest_score = []
-    
     for i, score in enumerate(scores):
         if score == max_score:
             highest_score.append(i+1)
