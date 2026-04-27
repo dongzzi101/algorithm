ea1a472c9e8ca7e48fd47c787b7d8125df759320
dongzzi101 <dongzzi101@gmail.com>
2026-04-27 14:39:08 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,31 +1,30 @@
 from collections import deque
 
-N = int(input())
-edges = int(input())
+n = int(input())
+nn = int(input())
 
-adjs = [[] for _ in range(N+1)]
+adjs = [[] for _ in range(n+1)]
 
-
-for _ in range(edges):
-	a, b = map(int, input().split())
+for _ in range(nn):
+	a,b = map(int, input().split())
 	adjs[a].append(b)
 	adjs[b].append(a)
 
+
 q = deque()
 q.append(1)
-
-visited = [False] * (N+1)
+visited = [False] * (n + 1)
 visited[1] = True
 
 count = 0
+
 while q:
 	node = q.popleft()
 
 	for adj in adjs[node]:
 		if not visited[adj]:
 			count += 1
-			q.append(adj)
 			visited[adj] = True
+			q.append(adj)
 
 print(count)
-
