f0500bd7fff8ba85b313eccbcdf71e8220f03f33
dongzzi101 <dongzzi101@gmail.com>
2026-04-27 19:14:34 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 764 ms, Memory: 43548 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,50 +1,45 @@
 from collections import deque
 
 n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
+def find_target():
+    for y in range(n):
+        for x in range(m):
+            if graph[y][x] == 2:
+                return y, x
 
-lands = [list(map(int, input().split())) for _ in range(n)]
-
-def find_number_two():
-	for y in range(n):
-		for x in range(m):
-			if lands[y][x] == 2:
-				return y, x
-
+sy, sx = find_target()
 
 q = deque()
-y, x = find_number_two()
-lands[y][x] = 0
-q.append((y, x))
+q.append((sy, sx))
 
+dist_map = [[-1] * m for _ in range(n)]
+dist_map[sy][sx] = 0
 
 visited = [[False] * m for _ in range(n)]
-visited[y][x] = True
-
+visited[sy][sx] = True
 
 dy = [1, 0, -1, 0]
 dx = [0, 1, 0, -1]
 
 while q:
+    y, x = q.popleft()
 
-	y, x = q.popleft()
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
 
-	for i in range(4):
-		ny = y + dy[i]
-		nx = x + dx[i]
-
-		if (0 <= ny < n) and (0 <= nx < m):
-			if not visited[ny][nx]:
-				if lands[ny][nx] != 0:
-					lands[ny][nx] = (lands[y][x] + 1)
-					q.append((ny, nx))
-					visited[ny][nx] = True
+        if 0 <= ny < n and 0 <= nx < m:
+            if not visited[ny][nx] and graph[ny][nx] != 0:
+                visited[ny][nx] = True
+                dist_map[ny][nx] = dist_map[y][x] + 1
+                q.append((ny, nx))
 
 for y in range(n):
     for x in range(m):
-        if lands[y][x] == 1 and not visited[y][x]:
-            lands[y][x] = -1	
-
-for land in lands:
-	print(*land)
-
+        if graph[y][x] == 0:
+            print(0, end=' ')
+        else:
+            print(dist_map[y][x], end=' ')
+    print()
