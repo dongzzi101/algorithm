7d4842dd8a46f705b812301546652a8db7d1e0df
dongzzi101 <dongzzi101@gmail.com>
2026-04-27 14:08:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.57 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,21 +1,14 @@
 from collections import deque
 
 def solution(maps):
-    row = len(maps)
-    col = len(maps[0])
-    
-    new_maps = [[0] * (col + 1)]
-    for r in maps:
-        new_maps.append([0] + r)
-    
-    if new_maps[1][1] == 0:
-        return -1
+    n = len(maps)
+    m = len(maps[0])
     
     q = deque()
-    q.append((1, 1, 1))
+    q.append((0, 0, 1))
     
-    visited = [[False] * (col+1) for _ in range(row+1)]
-    visited[1][1] = True
+    visited = [[False] * m for _ in range(n)]
+    visited[0][0] = True
     
     dy = [1, 0, -1, 0]
     dx = [0, 1, 0, -1]
@@ -23,16 +16,16 @@ def solution(maps):
     while q:
         y, x, dist = q.popleft()
         
-        if y == row and x == col:
+        if y == n - 1 and x == m - 1:
             return dist
         
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
             
-            if 1 <= ny <= row and 1 <= nx <= col:
-                if not visited[ny][nx] and new_maps[ny][nx] != 0:
+            if 0 <= ny < n and 0 <= nx < m:
+                if not visited[ny][nx] and maps[ny][nx] == 1:
                     visited[ny][nx] = True
-                    q.append((ny, nx, dist+1))
+                    q.append((ny, nx, dist + 1))
     
     return -1
