9a85bbdcdf8cff4f9512e0e9b84cc41e54f9d494
dongzzi101 <dongzzi101@gmail.com>
2026-04-28 14:42:43 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 676 ms, Memory: 67048 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,31 +1,38 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(start):
-    stack = [start]
-    visited[start] = True
+from collections import deque
 
-    while stack:
-        node = stack.pop()
-        for adj in adjs[node]:
-            if not visited[adj]:
-                visited[adj] = True
-                stack.append(adj)
+n, nn = map(int, input().split())
 
-N, M = map(int, input().split())
+adjs = [[] for _ in range(n+1)]
 
-adjs = [[] for _ in range(N+1)]
-for _ in range(M):
-    a, b = map(int, input().split())
-    adjs[a].append(b)
-    adjs[b].append(a)
+for _ in range(nn):
+	a, b = map(int, input().split())
+	adjs[a].append(b)
+	adjs[b].append(a)
 
-visited = [False] * (N+1)
+visited = [False] * (n+1)
 
-num_of_cc = 0
-for node in range(1, N+1):
-    if not visited[node]:
-        dfs(node)
-        num_of_cc += 1
+def bfs(node):
+	q = deque()
+	q.append(node)
+	visited[node] = True
 
-print(num_of_cc)
+	while q:
+		node = q.popleft()
+
+		for adj in adjs[node]:
+			if not visited[adj]:
+				q.append(adj)
+				visited[adj] = True
+
+count = 0
+for i in range(1, n+1):
+	if not visited[i]:
+		bfs(i)
+		count += 1
+
+print(count)
+
+
