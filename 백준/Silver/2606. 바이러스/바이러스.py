0341240d530073ea9868bb8b1ca58ca0b90c7105
dongzzi101 <dongzzi101@gmail.com>
2026-04-28 14:02:47 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -6,14 +6,14 @@ nn = int(input())
 adjs = [[] for _ in range(n+1)]
 
 for _ in range(nn):
-	a,b = map(int, input().split())
+	a, b = map(int, input().split())
 	adjs[a].append(b)
 	adjs[b].append(a)
 
-
 q = deque()
 q.append(1)
-visited = [False] * (n + 1)
+
+visited = [False] * (n+1)
 visited[1] = True
 
 count = 0
@@ -23,8 +23,8 @@ while q:
 
 	for adj in adjs[node]:
 		if not visited[adj]:
-			count += 1
-			visited[adj] = True
 			q.append(adj)
+			count += 1
+			visited[adj] = True	
 
 print(count)
