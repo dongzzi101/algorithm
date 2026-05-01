e517574862621410b7e97f8b941ce26cef1a1ced
dongzzi101 <dongzzi101@gmail.com>
2026-05-01 19:07:14 +0900

    [level 1] Title: K번째수, Time: 0.03 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,10 +1,15 @@
 def solution(array, commands):
     answer = []
     
+    
     for command in commands:
         start, end, target = command
-        new_arr = array[start-1 : end]
-        new_arr.sort()
-        answer.append(new_arr[target-1])
+        print(f"start = {start}, end = {end}, target = {target}")
+        new_array = array[start-1:end]
+        print(new_array)
+        new_array.sort()
+        answer.append(new_array[target-1])
+        print(f"sorted_array : {new_array}  target : {new_array[target-1]}")
+        array = array
     
     return answer
