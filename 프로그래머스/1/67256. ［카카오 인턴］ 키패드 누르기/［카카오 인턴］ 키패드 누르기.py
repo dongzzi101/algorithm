df4d1bd2dee6fdafff5d8716134d95f465cd4c94
dongzzi101 <dongzzi101@gmail.com>
2026-05-18 10:40:51 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.35 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,38 +1,42 @@
 def solution(numbers, hand):
     answer = ''
-    
+
     keypad = {
         1:(0,0), 2:(0,1), 3:(0,2),
         4:(1,0), 5:(1,1), 6:(1,2),
         7:(2,0), 8:(2,1), 9:(2,2),
         '*':(3,0), 0:(3,1), '#':(3,2)
     }
-    
+
+    def calc_dist(pos, target):
+        return abs(pos[0] - target[0]) + abs(pos[1] - target[1])
+
     left_pos = keypad['*']
     right_pos = keypad['#']
-    
+
     for number in numbers:
-        
-        if number in [1,4,7]:
+        if number in [1, 4, 7]:
             answer += 'L'
             left_pos = keypad[number]
-        
-        elif number in [3,6,9]:
+
+        elif number in [3, 6, 9]:
             answer += 'R'
             right_pos = keypad[number]
-        
+
         else:
             target = keypad[number]
-            
-            left_dist = abs(left_pos[0] - target[0]) + abs(left_pos[1] - target[1])
-            right_dist = abs(right_pos[0] - target[0]) + abs(right_pos[1] - target[1])
-            
+
+            left_dist = calc_dist(left_pos, target)
+            right_dist = calc_dist(right_pos, target)
+
             if left_dist < right_dist:
                 answer += 'L'
                 left_pos = target
-            elif right_dist < left_dist:
+
+            elif left_dist > right_dist:
                 answer += 'R'
                 right_pos = target
+
             else:
                 if hand == 'left':
                     answer += 'L'
@@ -40,5 +44,5 @@ def solution(numbers, hand):
                 else:
                     answer += 'R'
                     right_pos = target
-    
+
     return answer
