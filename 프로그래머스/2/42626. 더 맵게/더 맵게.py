5367e9ce1c488f2704ddfc5907c2f785190612a2
dongzzi101 <dongzzi101@gmail.com>
2026-05-21 12:36:00 +0900

    [level 2] Title: 더 맵게, Time: 1185.96 ms, Memory: 50.5 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,19 +1,21 @@
-import heapq
+import heapq 
 
 def solution(scoville, K):
     heapq.heapify(scoville)
+    
     count = 0
-
-    while scoville[0] < K:
-        if len(scoville) < 2:
+    while scoville:
+        min_val = heapq.heappop(scoville)
+        
+        if min_val >= K:
+            return count
+        
+        if len(scoville) == 0:
             return -1
         
-        first = heapq.heappop(scoville)
-        second = heapq.heappop(scoville)
-        
-        new = first + (second * 2)
-        heapq.heappush(scoville, new)
-        
+        next_min_val = heapq.heappop(scoville)
+        new_val = min_val + (next_min_val * 2)
+        heapq.heappush(scoville, new_val)
         count += 1
-
-    return count
+    
+    return -1
