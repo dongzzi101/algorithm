7cad593774f44eea0411805db056bef473467bd2
dongzzi101 <dongzzi101@gmail.com>
2026-05-28 14:59:16 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 21.29 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -3,30 +3,38 @@ def solution(m, n, board):
     answer = 0
     
     while True:
-        to_remove = set()
+        pang = set()
         
         for y in range(m-1):
             for x in range(n-1):
-                if board[y][x] == '.':
-                    continue
-                
-                if board[y][x] == board[y][x+1] == board[y+1][x] == board[y+1][x+1]:
-                    print((y,x), (y,x+1), (y+1,x), (y+1,x+1))
-                    to_remove.update([
-                        (y,x), (y,x+1),
-                        (y+1,x), (y+1,x+1)
-                    ])
+                target = board[y][x]
+                if target != '0' and board[y][x] == board[y+1][x] == board[y][x+1] == board[y+1][x+1]:
+                    pang.add((y, x))
+                    pang.add((y+1, x))
+                    pang.add((y, x+1))
+                    pang.add((y+1, x+1))
         
-        if not to_remove:
+        if not pang:  
             break
-        
-        answer += len(to_remove)
-        for y, x in to_remove:
-            board[y][x] = '.'
-        
-        for x in range(n):
-            stack = [board[y][x] for y in range(m) if board[y][x] != '.']
-            for y in range(m-1, -1, -1):
-                board[y][x] = stack.pop() if stack else '.'
-    
+            
+        answer += len(pang)
+        for y, x in pang:
+            board[y][x] = '0'
+            
+        for x in range(n): 
+            stack = []
+            
+            
+            for y in range(m):
+                if board[y][x] != '0':
+                    stack.append(board[y][x])
+            
+            empty_count = m - len(stack)
+            
+            for y in range(m):
+                if y < empty_count:
+                    board[y][x] = '0' 
+                else:
+                    board[y][x] = stack[y - empty_count]
+            
     return answer
