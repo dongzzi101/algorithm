b56267884a1be8ea37a5b32f18060cc8844a6f57
dongzzi101 <dongzzi101@gmail.com>
2026-05-29 13:38:31 +0900

    [level 1] Title: 추억 점수, Time: 0.88 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,17 +1,22 @@
 def solution(name, yearning, photo):
     answer = []
+    
     yearning_map = {}
     
     for i in range(len(name)):
-        yearning_map[name[i]] = yearning[i]
+        if name[i] in yearning_map:
+            yearning_map[name[i]] += 0
+        else:
+            yearning_map[name[i]] = yearning[i]
     
-    score = 0
+    
+    
     for y in range(len(photo)):
+        result = 0
         for x in range(len(photo[y])):
             if photo[y][x] in yearning_map:
-                score += yearning_map[photo[y][x]]
+                result += yearning_map[photo[y][x]]
         
-        answer.append(score)
-        score = 0
+        answer.append(result)
     
     return answer
