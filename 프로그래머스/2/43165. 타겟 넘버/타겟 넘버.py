503b215a843b5f5c840e8fef1e4ef9a17d25b270
dongzzi101 <dongzzi101@gmail.com>
2026-05-31 21:03:26 +0900

    [level 2] Title: 타겟 넘버, Time: 135.23 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,13 +1,15 @@
 def solution(numbers, target):
-    def dfs(numbers, target, depth, current_sum):
-        if depth == len(numbers):
+    def dfs(index, current_sum):
+        if index == len(numbers):
             if current_sum == target:
                 return 1
             else:
                 return 0
-        return (
-            dfs(numbers, target, depth+1, current_sum + numbers[depth]) +
-            dfs(numbers, target, depth+1, current_sum - numbers[depth])
-        )
     
-    return dfs(numbers, target, 0, 0)
+    
+        add_case = dfs(index + 1, current_sum + numbers[index])
+        sub_case = dfs(index + 1, current_sum - numbers[index])
+    
+        return add_case + sub_case
+    
+    return dfs(0,0)
