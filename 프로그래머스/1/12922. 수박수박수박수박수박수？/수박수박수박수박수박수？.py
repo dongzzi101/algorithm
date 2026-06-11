ba95440c8b8b22ccb84701c70213233446fb84f0
dongzzi101 <dongzzi101@gmail.com>
2026-06-11 12:59:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.45 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -2,9 +2,9 @@ def solution(n):
     answer = ''
     
     for i in range(n):
-        if i % 2 != 0:
-            answer += "박"
+        if i % 2 == 0:
+            answer += '수'
         else:
-            answer += "수"
+            answer += '박'
     
     return answer
