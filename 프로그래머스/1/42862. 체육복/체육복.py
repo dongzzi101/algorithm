2b61619f0123c518aa2562252dd7c72d56e14ea9
dongzzi101 <dongzzi101@gmail.com>
2026-06-14 17:11:26 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,19 +1,31 @@
 def solution(n, lost, reserve):
     answer = 0
     
-    lost = set(lost)
-    reserve = set(reserve)
-
-    both = lost & reserve
-    lost -= both
-    reserve -= both
+    students = [i+1 for i in range(n)]
     
-    for r in sorted(reserve):
-        if r-1 in lost:
-            lost.remove(r-1)
-        elif r+1 in lost:
-            lost.remove(r+1)
+    students_map = {}
     
-    answer = n - len(lost)
+    for student in students:
+        students_map[student] = 0
     
+    for lost_student in lost:
+        students_map[lost_student] -= 1
+    
+    for reserve_student in reserve:
+        students_map[reserve_student] += 1
+    
+    
+    for sm in students_map:
+        if students_map[sm] < 0:
+            if sm - 1 in students_map and students_map[sm - 1] > 0:
+                students_map[sm] += 1
+                students_map[sm - 1] -= 1
+            elif sm + 1 in students_map and students_map[sm + 1] > 0:
+                students_map[sm] += 1
+                students_map[sm + 1] -= 1
+        
+    for sm in students_map:
+        if students_map[sm] >= 0:
+            answer += 1
+        
     return answer
