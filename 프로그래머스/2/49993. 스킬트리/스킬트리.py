3373e2acfc3b9701b9855e78707b29557ffdaf12
dongzzi101 <dongzzi101@gmail.com>
2026-06-15 15:04:59 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,16 +1,16 @@
 def solution(skill, skill_trees):
     answer = 0
     
-    for skill_tree in skill_trees:
-        temp = []
-        for sk in skill_tree:
-            if sk in skill:
-                temp.append(sk)
-        
-        extracted_skill = "".join(temp)
-        
-        if extracted_skill == skill[:len(temp)]:
-            answer += 1
     
+    for skill_tree in skill_trees:
+        remain_skill = []
+        
+        for st in skill_tree:
+            if st not in skill:
+                continue
+            remain_skill.append(st)
+        
+        if "".join(remain_skill) == skill[:len(remain_skill)]:
+            answer += 1
     
     return answer
