167fd2b67ef1bc27ccff4700429c55e239f88cd0
dongzzi101 <dongzzi101@gmail.com>
2026-06-25 20:43:14 +0900

    [level 1] Title: 소수 만들기, Time: 7.94 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,25 +1,20 @@
 from itertools import combinations
-from math import sqrt
-
-def is_prime(num):
-    return (len(get_divisors(num)) == 2)
-    
-
-def get_divisors(num):
-    s = set()
-    for i in range(1, int(sqrt(num)) + 1):
-        if num % i == 0:
-            s.add(i)
-            s.add(num // i)
-    return s
-    
 
 def solution(nums):
-    answer = 0
-    count = 0
-    for i in combinations(nums, 3):
-        if is_prime(sum(i)):
-            count += 1 
     
-    answer = count
-    return answer
+    def is_prime_number(num):
+        
+        for n in range(2, int(num ** 0.5) + 1):
+            if num % n == 0:
+                return False
+        
+        return True
+    
+    count = 0
+    
+    for num in combinations(nums, 3):
+        
+        if is_prime_number(sum(num)):
+            count += 1
+
+    return count
