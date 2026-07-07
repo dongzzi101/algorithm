378a768203a0fa8e16e18b47327063d150146e05
dongzzi101 <dongzzi101@gmail.com>
2026-07-07 10:54:48 +0900

    [level 1] Title: 소수 만들기, Time: 7.84 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,20 +1,16 @@
 from itertools import combinations
 
 def solution(nums):
+    answer = 0
     
-    def is_prime_number(num):
-        
+    def is_prime_num(num):
         for n in range(2, int(num ** 0.5) + 1):
             if num % n == 0:
                 return False
-        
         return True
     
-    count = 0
-    
-    for num in combinations(nums, 3):
+    for comb in combinations(nums, 3):
+        if is_prime_num(sum(comb)):
+            answer += 1
         
-        if is_prime_number(sum(num)):
-            count += 1
-
-    return count
+    return answer
